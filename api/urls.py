6e0493af9ca537d29561d66b64d5e87f8c5ee4a1
JuliusValentineI <hernanigonzalez.personal@gmail.com>
2024-02-27 14:01:41 -0600

    Front: Create [slug].tsx in blog folder and header.tsx component

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import ArticlesViewSet, FilesViewSet
+from .views import ArticlesViewSet, FilesViewSet, showFile
 
 router = routers.DefaultRouter()
 router.register(r'articles', ArticlesViewSet)
 router.register(r'files', FilesViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('showFile/', showFile, name="Show"),
+    path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
 #from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 from rest_framework import viewsets
+#from rest_framework.mixins import Response
 #from rest_framework.response import Response
 from .models import Articles, Files
 from .serialize import ArticlesSerializer, FilesSerializer
 """
---- PostsRepository methods 
+--- Article and views methods 
 --- Views in database created's
 """
     
@@ -17,3 +19,25 @@ class FilesViewSet(viewsets.ModelViewSet):
     queryset = Files.objects.all()
     serializer_class = FilesSerializer
     
+
+def showFile(request):
+    try:
+        articleId = int(request.GET['article'])
+        file = Files.objects.filter(article=articleId).first()
+
+        data = {
+            "id": file.id,
+            "filename": file.filename,
+            "filetype": file.filetype,
+            "content": file.content,
+            "article": articleId
+        }
+
+        if file == None:
+            return JsonResponse({"error": "File not found"})
+       
+        return JsonResponse(data)
+    except ValueError:
+        return JsonResponse({"error": "Invalid article id format"})
+
+
